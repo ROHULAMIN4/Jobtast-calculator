b569eec77b3698c76b1b421dd616d648e81b872c
Rohul Amin <rohulamin83716@gmail.com>
2022-01-14 20:32:56 +0600

    mobile diler keypad

---
+++ Calculaor/js/App.js
@@ -0,0 +1,13 @@
+document.getElementById("key-pad").addEventListener("click", function (event) {
+  const number = event.target.innerText;
+  const calcInput = document.getElementById("typed-numbers");
+  if (isNaN(number)) {
+    if (number == "C") {
+      calcInput.value = "";
+    }
+  } else {
+    const previousNumber = calcInput.value;
+    const newNumber = previousNumber + number;
+    calcInput.value = newNumber;
+  }
+});
